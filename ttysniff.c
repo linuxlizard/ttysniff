2b5871482a11f6ead5a7e1e795cd834046ab3b90
David Poole <testcluster@gmail.com>
2014-01-06 12:28:25 -0700

    initial checkin of pre-existing code

---
+++ ttysniff.c
@@ -0,0 +1,584 @@
+/*****************************************************
+ *    Copyright 2002 David Poole
+ *    Source File            :  ttysniff.c
+ *    Created By             :  David Poole
+ *    Date Created           :  ~2001
+ *    Platform Dependencies  :  POSIX, Linux
+ *    Description            :
+ *      A simple program to read from a serial port and dump
+ *      the data to stdout.  Useful since it can dump in hex
+ *      and set serial port settings.
+ *
+ *    Reference Documentation:
+ ******************************************************/
+
+/* $Id: ttysniff.c 560 2010-03-12 16:22:11Z davep $ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/select.h>
+#include <ctype.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define VERSION "2.0.0"
+
+/* values for parity */
+#define SERIAL_NO_PARITY    1
+#define SERIAL_EVEN_PARITY  2
+#define SERIAL_ODD_PARITY   4
+
+/* values for data bits */
+#define SERIAL_8_DATA_BITS  8 
+#define SERIAL_7_DATA_BITS  7 
+
+/* values for stop bits */
+#define SERIAL_1_STOP_BITS  1 
+#define SERIAL_2_STOP_BITS  2 
+
+#define FALSE 0
+#define TRUE  1
+
+#define DEFAULT_BAUD_RATE  B9600
+//#define BAUD_RATE  B19200
+//#define BAUD_RATE  B38400
+            
+#define NON_BLOCKING_SERIAL 
+
+char serial_port[FILENAME_MAX+1];
+char logfile_name[FILENAME_MAX+1];
+
+int opt_log_output = FALSE; /* log output to file */
+int opt_hex_output = TRUE;  /* dump output as hex */
+
+speed_t baudrate = DEFAULT_BAUD_RATE;
+
+/* at some point, could make these command line configurable */
+int parity = SERIAL_NO_PARITY;
+//int databits = SERIAL_7_DATA_BITS;
+int databits = SERIAL_8_DATA_BITS;
+int stopbits = SERIAL_1_STOP_BITS;
+
+int main_quit = 0;
+
+typedef struct {
+    const char *str;
+    int len; /* string length of str */
+    speed_t speed;
+} BaudRate;
+
+BaudRate baudrates[] = {
+    { "110",  3, B110   },
+    { "150",  3, B150   },
+    { "300",  3, B300   },
+    { "600",  3, B600   },
+    { "1200", 4, B1200  },
+    { "2400", 4, B2400  },
+    { "4800", 4, B4800  },
+    { "9600", 4, B9600  },
+    { "19200",  5, B19200 },
+    { "38400",  5, B38400 },
+    { "57600",  5, B57600 },
+    { "115200", 6, B115200},
+    { "230400", 6, B230400},
+};
+
+const unsigned char CR = '\r';
+const unsigned char LF = '\n';
+
+#if defined linux || defined unix || defined __GNUC__
+void signal_term( int signum )
+{
+    fprintf( stderr, "signal!\n" );
+    main_quit = 1;
+}
+
+void init_signals( void )
+{
+    int uerr;
+    struct sigaction sigterm; 
+    struct sigaction sigint; 
+
+    memset( &sigterm, 0, sizeof(sigterm) );
+    sigterm.sa_handler = signal_term;
+    uerr = sigaction( SIGTERM, &sigterm, NULL );
+    if( uerr != 0 ) {
+        perror( "sigaction(SIGTERM)" );
+        exit(1);
+    }
+
+    memset( &sigint, 0, sizeof(sigint) );
+    sigint.sa_handler = signal_term;
+    uerr = sigaction( SIGINT, &sigint, NULL );
+    if( uerr != 0 ) {
+        perror( "sigaction(SIGINT)" );
+        exit(1);
+    }
+}
+#endif
+
+int serial_open_port( void )
+{
+    struct termios oldtio, newtio;
+    int fd;
+
+    /* stupid human check -- I've been running this thing 
+     *  as root (very very dumb) so I don't want to accidently
+     *  write DNP to /dev/hda1 or something equally tragic.
+     */
+//    if( strncmp( path, "/dev/ttyS", 9 ) != 0 ) {
+//        LOG_MESSAGE1( LOG_ERR, "\"%s\" not a serial port.", path );        
+//        return ERR_FAIL;
+//    }
+
+    /* davep 29-Apr-2009 ; temp debug */
+    fprintf( stderr, "%s opening %s\n", __FUNCTION__, serial_port );
+
+    /* open serial port */
+#ifdef NON_BLOCKING_SERIAL
+    fd = open( serial_port, O_RDWR|O_NOCTTY|O_NONBLOCK );
+#else
+    fd = open( serial_port, O_RDWR|O_NOCTTY );
+#endif
+    if( fd < 0 ) {
+        fprintf( stderr, "open() of %s failed : %s", 
+                serial_port, strerror(errno) );
+        return -1;
+    }
+
+    /* davep 29-Apr-2009 ; temp debug */
+    fprintf( stderr, "%s opened %s successfully\n", __FUNCTION__, serial_port );
+
+    tcgetattr( fd, &oldtio );
+
+    memset( &newtio, 0, sizeof(newtio) );
+
+    newtio.c_cflag = CLOCAL | CREAD ;
+
+    /* parity */
+    switch( parity ) {
+        case SERIAL_NO_PARITY :
+            /* nothing -- default is no parity */
+            break;
+
+        case SERIAL_EVEN_PARITY :
+            newtio.c_cflag |= PARENB;
+            break;
+
+        case SERIAL_ODD_PARITY :
+            newtio.c_cflag |= PARENB | PARODD;
+            break;
+
+        default :
+            assert( 0 );
+    }
+
+    /* data bits */
+    if( databits == SERIAL_8_DATA_BITS ) {
+        newtio.c_cflag |= CS8;
+    }
+    else if( databits == SERIAL_7_DATA_BITS ) {
+        newtio.c_cflag |= CS7;
+    }
+    else {
+        assert( 0 );
+    }
+
+    /* stop bits */
+    if( stopbits == SERIAL_1_STOP_BITS ) {
+        /* nothing -- default is 1 stop bit */
+    }
+    else if( stopbits == SERIAL_2_STOP_BITS ) {
+        newtio.c_cflag |= CSTOPB;
+    }
+    else {
+        assert( 0 );
+    }
+
+
+    newtio.c_lflag = 0;
+    newtio.c_iflag = 0; // | IXON | IXANY | IXOFF | IMAXBEL;
+    newtio.c_oflag = 0;
+
+    cfsetospeed( &newtio, baudrate );
+    cfsetispeed( &newtio, baudrate );
+
+#ifndef NON_BLOCKING_SERIAL
+    newtio.c_cc[VMIN]  = 1; /* block until this many chars recvd */
+    newtio.c_cc[VTIME] = 0; /* inter character timer unused */
+#endif
+
+    tcflush( fd, TCIFLUSH );
+    tcsetattr( fd, TCSANOW, &newtio );
+
+    return fd;
+}
+
+int baud_string_to_baud( char *baudstring, speed_t *baudrate )
+{
+    int i;
+
+    for( i=0 ; i<sizeof(baudrates)/sizeof(BaudRate) ; i++ ) {
+        if(strncasecmp(baudrates[i].str,baudstring,baudrates[i].len )==0) {
+            /* found it */
+            *baudrate = baudrates[i].speed;
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+const char *baud_to_baud_string( speed_t baudrate )
+{
+    int i;
+
+    for( i=0 ; i<sizeof(baudrates)/sizeof(BaudRate) ; i++ ) {
+        if( baudrates[i].speed == baudrate ) {
+            /* found it */
+            return baudrates[i].str;
+        }
+    }
+
+    return NULL;
+}
+
+void print_usage( void )
+{
+    int i;
+
+    printf( "ttysniff %s (%s) - read and dump data from serial port.\n", 
+            VERSION, __DATE__ );
+    printf( "usage: ttysniff [-options] [-l logfile] [-b baud] path\n" );
+    printf( "  -h : show help\n" );
+    printf( "  -p : print data as readable (default is to print as hex)\n" );
+    printf( "  -l logfile : log traffic to binary file\n" );
+    printf( "  -b baudrate : set baud rate (default=%s)\n", 
+            baud_to_baud_string(DEFAULT_BAUD_RATE) );
+    printf( "  -7 : 7 data bits\n" );
+    printf( "  -8 : 8 data bits (default)\n" );
+    printf( "  path : path to serial port (/dev/ttyS?)\n" );
+    printf( "\n" );
+    printf( "Available baud rates are: " );
+
+    for( i=0 ; i<sizeof(baudrates)/sizeof(BaudRate) ; i++ ) {
+        printf( "%s ", baudrates[i].str );
+    }
+    printf( "\n" );
+}
+
+void parse_args( int argc, char *argv[] )
+{
+    char c;
+
+    if( argc == 1 ) {
+        print_usage();
+        exit(1);
+    }
+
+    baudrate = DEFAULT_BAUD_RATE;
+
+    while( 1 ) {
+        c = getopt( argc, argv, "hpl:b:78" );
+
+        if( c==-1 )
+            break;
+
+        switch( c ) {
+            case 'h' :
+                print_usage();
+                exit(1);
+                break;
+
+            case 'l' :
+                /* save output file name */
+                strncpy( logfile_name, optarg, FILENAME_MAX );
+                opt_log_output = TRUE;
+                break;
+
+            case 'p' :
+                opt_hex_output = FALSE;
+                break;
+
+            case 'b' :
+                /* convert baud rate string to baud rate number */
+                if( baud_string_to_baud( optarg, &baudrate ) != 0 ) {
+                    fprintf( stderr, "Invalid baud rate \"%s\"\n", optarg );
+                    exit(1);
+                }
+                break;
+
+            case '7' :
+                /* 7 data bits */
+                databits = SERIAL_7_DATA_BITS;
+                break;
+
+            case '8' :
+                /* 8 data bits */
+                databits = SERIAL_8_DATA_BITS;
+                break;
+
+            case '?' :
+                print_usage();
+                exit(1);
+                break;
+
+            default :
+                break;
+        }
+    }
+
+    
+    /* get serial port name */
+    if (optind < argc) {
+        strncpy( serial_port, argv[optind++], FILENAME_MAX );
+    }
+    else {
+        fprintf( stderr, "You must specify a serial port.\n" );
+        print_usage();
+        exit(1);
+    }
+
+    /* XXX temp debug */
+//    fprintf( stderr, "baud=%ld port=%s\n", baudrate, serial_port );
+
+}
+
+int
+terminal_to_raw( struct termios *save_tios )
+{
+    struct termios newtios;
+    int stdin_fileno;
+    int retcode;
+
+    stdin_fileno = fileno(stdin);
+
+    /* get a copy well save until we restore stdin at exit */
+    retcode = tcgetattr( stdin_fileno, save_tios );
+    if( retcode < 0 ) {
+        fprintf( stderr, "tcgetattr() failed to get stdin term attributes: %d %s\n", 
+                errno, strerror(errno));
+        return -1;
+    }
+
+    /* get them again so we can muck with them */
+    retcode = tcgetattr( stdin_fileno, &newtios );
+    if( retcode < 0 ) {
+        fprintf( stderr, "tcgetattr() failed to get stdin term attributes: %d %s\n", 
+                errno, strerror(errno));
+        return -1;
+    }
+
+    cfmakeraw( &newtios );
+
+    /* set stdin to raw */
+    retcode = tcsetattr( stdin_fileno, TCSANOW, &newtios );
+    if( retcode < 0 ) {
+        fprintf( stderr, "tcsetattr() failed to set stdin term attributes: %d %s\n", 
+                errno, strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+int main( int argc, char *argv[] )
+{
+    int err;
+    FILE *binfile=NULL;
+    int stdin_fileno, fd;
+    unsigned char databyte;
+    int count;
+    fd_set read_fds;
+    int max_fd;
+    struct termios stdin_tios;
+    
+    parse_args( argc, argv );
+
+    init_signals();
+
+    if( opt_log_output ) {
+        binfile = fopen( logfile_name, "w" );
+        if( binfile == NULL ) {
+            fprintf( stderr, "Unable to open log file \"%s\" : %s\n", 
+                    logfile_name, strerror(errno) );
+            exit(1);
+        }
+        setbuf( binfile, NULL );
+    }
+
+    fd = serial_open_port();
+    if( fd < 0 ) {
+        /* serial_open_port() displays error */
+        exit(1);
+    }
+
+    /* set the terminal to raw so we can get single keystrokes */
+    fprintf( stderr, "setting stdin to raw...\n" );
+    memset( &stdin_tios, 0, sizeof(struct termios) );
+    terminal_to_raw( &stdin_tios );
+
+    /* davep 20-July-06 ; XXX TEMPORARY FOR DEBUGGING kick off some activity */
+//    write( fd, "scan var print\r\n", 16 );
+
+    setbuf( stdout, NULL );
+    count = 0;
+    stdin_fileno = fileno(stdin);
+    while( !main_quit ) {
+
+//        fprintf( stderr, "stdin=%d\n", fileno(stdin));
+
+        FD_ZERO( &read_fds );
+        FD_SET( stdin_fileno, &read_fds );
+        FD_SET( fd, &read_fds );
+
+        max_fd = stdin_fileno;
+        if( fd > max_fd ) {
+            max_fd = fd;
+        }
+
+        err = select( max_fd+1, &read_fds, NULL, NULL, NULL );
+        if( err<0 ) {
+            fprintf( stderr, "select err=%d errno=%d (%s)\r\n", err, errno,
+                    strerror(errno) );
+            continue;
+        }
+        
+        if( FD_ISSET( stdin_fileno, &read_fds ) ) {
+            err = read( stdin_fileno, &databyte, 1 );
+            if( err <= 0 ) {
+                fprintf( stderr, "read stdin err=%d errno=%d (%s)\r\n", 
+                        err, errno, strerror(errno) );
+                break;
+            }
+
+//            printf( "%#04x ", databyte );
+
+            if( databyte == 0x03 ) {
+                /* Ctrl-C? */
+                break;
+            }
+
+            write( fd, &databyte, 1 );
+            if( databyte==CR ) {
+                write( fd, &LF, 1 );
+            }
+        }
+
+        if( FD_ISSET( fd, &read_fds ) ) {
+            err = read( fd, &databyte, 1 );
+            if( err <= 0 ) {
+                fprintf( stderr, "read fd err=%d errno=%d (%s)\r\n", 
+                        err, errno, strerror(errno) );
+                break;
+            }
+
+            if( opt_hex_output ) {
+                /* There is something weird in printf().  I want to see 0x00 and it
+                 * prints "0" instead.  Messes up my pretty screen alignment.
+                 */
+                if( databyte ) {
+#ifdef PRINT_0X
+                    printf( "%#04x ", databyte );
+                }
+                else {
+                    printf( "0x00 " );
+                }
+#else
+                    printf( "%02x ", databyte );
+                }
+                else {
+                    printf( "00 " );
+                }
+#endif
+            }
+            else {
+                /* buyer beware -- just print whatever we get */
+                /* davep 01-Aug-2010 ; I did the 0x7f a while ago (strips the
+                 * 8th bit) because our newer hardware is fucked up and the 8th
+                 * bit flips up and down randomly.
+                 */
+//                printf( "%c", databyte&0x7f );
+
+                /* Adding the iscntrl() to get rid of the goddamn mother
+                 * fucking ^M in my output files
+                 */
+//                if( !iscntrl( databyte ) && databyte!=CR ) {
+//                    printf( "%c", databyte&0x7f );
+//                }
+//                else if( databyte==LF ) {
+//                    printf( "\r\n" );
+//                }
+//                printf( "%c", databyte );
+
+                int printit = 1;
+
+                /* davep 10-May-2012 ; fucking bell pissing me off */
+                if( databyte==0x07 ) {
+                    printit = 0;
+                }
+
+                /* davep 19-aug-2011 ; Yukon uses \n\r not \r\n (WTF!?) so
+                 * filter out the stupid-ass 0x0d 
+                 */
+                if( databyte==0x0a ) {
+                    printf( "%c", 0x0d );
+                }
+                if( databyte==0x0d ) {
+                    printit = 0;
+                }
+
+                /* davep 27-Oct-2011 ; quiet down people doing shit like:
+                 *
+                 *  ******** color_pipe_set_config ENTRY config: 0x5fae64
+                 *
+                 */
+                if( databyte == '*' ) {
+                    printit = 0;
+                }
+
+                if( printit ) {
+                    printf( "%c", databyte&0x7f );
+                }
+            }
+
+
+            if( opt_log_output ) {
+                fwrite( &databyte, 1, 1, binfile );
+                fflush( binfile );
+            }
+        }
+
+        count++;
+    }
+
+    close( fd );
+    
+    if( opt_log_output ) {
+        fclose( binfile );
+    }
+
+    /* set stdin back to starting point */  
+    fprintf( stderr, "restoring stdin to previous state\r\n" );
+    err = tcsetattr( stdin_fileno, TCSANOW, &stdin_tios );
+    if( err < 0 ) {
+        fprintf( stderr, "tcsetattr() failed to set stdin term attributes: %d %s\n", 
+                errno, strerror(errno));
+        /* keep going and hope for the best */
+    }
+
+    printf( "\nbye!\n" );
+    return 0;
+}
+
---
+++ ttysniff.py
@@ -0,0 +1,239 @@
+#!/usr/bin/python
+
+import sys
+import os
+import termios
+
+import hexdump
+
+opt_hex_output = 0
+
+#port = "/dev/ttyUSB0"
+#port = "/dev/ttyS0"
+baud = 115200 
+
+def xassert( flag, value ) :
+    if not flag :
+        raise AssertionError( value )
+
+# lifted the names from the Linux termios manpage
+iflag_names = {
+    termios.IGNBRK: "IGNBRK",
+    termios.BRKINT: "BRKINT",
+    termios.IGNPAR: "IGNPAR",
+    termios.PARMRK: "PARMRK",
+    termios.INPCK: "INPCK",
+    termios.ISTRIP: "ISTRIP",
+    termios.INLCR: "INLCR",
+    termios.IGNCR: "IGNCR",
+    termios.ICRNL: "ICRNL",
+#    termios.IUCLC: "IUCLC",
+    termios.IXON: "IXON",
+    termios.IXANY: "IXANY",
+    termios.IXOFF: "IXOFF",
+    termios.IMAXBEL: "IMAXBEL",
+}
+
+oflag_names = { 
+    termios.OPOST: "OPOST",
+#    termios.OLCUC: "OLCUC",
+    termios.ONLCR: "ONLCR",
+    termios.OCRNL: "OCRNL",
+    termios.ONOCR: "ONOCR",
+    termios.ONLRET: "ONLRET",
+    termios.OFILL: "OFILL",
+    termios.OFDEL: "OFDEL",
+    termios.NLDLY: "NLDLY",
+    termios.CRDLY: "CRDLY",
+    termios.TABDLY: "TABDLY",
+    termios.BSDLY: "BSDLY",
+    termios.VTDLY: "VTDLY",
+    termios.FFDLY: "FFDLY",
+}
+
+cflag_names = {
+#    termios.CBAUD: "CBAUD",
+#    termios.CBAUDEX: "CBAUDEX",
+    termios.CSIZE: "CSIZE",
+    termios.CSTOPB: "CSTOPB",
+    termios.CREAD: "CREAD",
+    termios.PARENB: "PARENB",
+    termios.PARODD: "PARODD",
+    termios.HUPCL: "HUPCL",
+    termios.CLOCAL: "CLOCAL",
+#    termios.LOBLK: "LOBLK",
+#    termios.CIBAUD: "CIBAUD",
+    termios.CRTSCTS: "CRTSCTS",
+}
+
+lflag_names = {
+    termios.ISIG: "ISIG",
+    termios.ICANON: "ICANON",
+#    termios.XCASE: "XCASE",
+    termios.ECHO: "ECHO",
+    termios.ECHOE: "ECHOE",
+    termios.ECHOK: "ECHOK",
+    termios.ECHONL: "ECHONL",
+    termios.ECHOCTL: "ECHOCTL",
+    termios.ECHOPRT: "ECHOPRT",
+    termios.ECHOKE: "ECHOKE",
+#    termios.DEFECHO: "DEFECHO",
+    termios.FLUSHO: "FLUSHO",
+    termios.NOFLSH: "NOFLSH",
+    termios.TOSTOP: "TOSTOP",
+    termios.PENDIN: "PENDIN",
+    termios.IEXTEN: "IEXTEN",
+}
+
+baudrate_names = {
+    termios.B0: "B0",
+    termios.B50: "B50",
+    termios.B75: "B75",
+    termios.B110: "B110",
+    termios.B150: "B150",
+    termios.B300: "B300",
+    termios.B1200: "B1200",
+    termios.B2400: "B2400",
+    termios.B4800: "B4800",
+    termios.B9600: "B9600",
+    termios.B19200: "B19200",
+    termios.B38400: "B38400",
+    termios.B57600: "B57600",
+    termios.B115200: "B115200",
+}
+
+def get_baudrate_name( value ) :
+    if baudrate_names.has_key( value ) :
+        return baudrate_names[ value ]
+    else :
+        return "(unknown baud rate)"
+
+def get_flag_names( value, flag_names ) :
+    setflags = []
+    for k in flag_names.keys() :
+        if value & k :
+            setflags.append( flag_names[k] )
+    return "|".join( setflags )
+
+def get_char_size( cflag ) :
+    charsize = cflag & termios.CSIZE
+
+    if cflag & termios.CSIZE :
+        if charsize == termios.CS5 :
+            return "5bit char"
+        elif charsize == termios.CS6 :
+            return "6bit char"
+        elif charsize == termios.CS7 :
+            return "7bit char"
+        elif charsize == termios.CS8 :
+            return "8bit char"
+        else :
+            xassert( 0, cflags & termios.CSIZE )
+    else :
+        return "CSIZE not set"        
+
+def print_tty_attr( tty_attr ) :
+    # tty_attr = [iflag, oflag, cflag, lflag, ispeed, ospeed, cc]
+    iflag = tty_attr[0]
+    print "iflag=%#x %s" % (iflag,get_flag_names(iflag,iflag_names))
+
+    oflag = tty_attr[1]
+    print "oflag=%#x %s" % (oflag,get_flag_names(oflag,oflag_names))
+
+    cflag = tty_attr[2]
+    print "cflag=%#x %s" % (cflag,get_flag_names(cflag,cflag_names))
+    charsize = cflag & termios.CSIZE
+    print "termios.CSIZE=%#x charsize=%#x (%s)" % (termios.CSIZE,charsize,get_char_size(cflag))
+
+    lflag = tty_attr[3]
+    print "lflag=%#x %s" % (lflag, get_flag_names(lflag,lflag_names))
+
+    ispeed = tty_attr[4]
+    name = get_baudrate_name( ispeed )
+    print "ispeed=%#x %s" % (ispeed,name)
+
+    ospeed = tty_attr[5]
+    name = get_baudrate_name( ospeed )
+    print "ospeed=%#x %s" % (ospeed,name)
+
+    cc = tty_attr[6]
+#    print type(cc),len(cc)
+#    print cc
+
+def serial_setup( port_path ) :
+    fd = os.open( port_path, os.O_RDWR|os.O_NOCTTY|os.O_SYNC)
+    # TODO add error checking! also probably should set the baud rate, etc
+    assert fd >= 0 
+
+    # set to 115200 
+    # returns [iflag, oflag, cflag, lflag, ispeed, ospeed, cc]
+    attr = termios.tcgetattr( fd )
+    print_tty_attr( attr )
+
+    # davep 20-July-06 ; these settings seem to work with our firmware
+    # iflag
+    attr[0] = termios.IGNBRK
+    # oflag
+    attr[1] = 0
+    # cflag
+#    attr[2] = termios.CBAUDEX|termios.CREAD|termios.CBAUD|termios.CSIZE|termios.CLOCAL
+    attr[2] = termios.CSIZE|termios.CLOCAL
+    # lflag
+    attr[3] = 0
+    
+    # baud rate    
+    attr[4] = termios.B115200
+    attr[5] = termios.B115200
+
+    # attr[6] is cc ; leave alone 
+
+    # push the settings to the serial port
+    termios.tcsetattr( fd, termios.TCSANOW, attr )
+
+    # retrieve and print to make sure they stuck
+    attr = termios.tcgetattr( fd )
+    print_tty_attr( attr )
+
+    return fd
+
+if __name__ == '__main__' :
+    if len(sys.argv) != 2 :
+        print >>sys.stderr, "usage: %s ttypath" % sys.argv[0]
+        sys.exit(1)
+
+    port = sys.argv[1]
+
+    fd = serial_setup( port )
+#    os.close(fd)
+#    sys.exit(1)
+
+    #fd = os.open( port, os.O_RDWR|os.O_NOCTTY|os.O_NONBLOCK )
+    #fd = os.open( port, os.O_RDWR|os.O_NOCTTY )
+#    fd = os.open( port, os.O_RDWR|os.O_NOCTTY|os.O_SYNC)
+
+    # returns [iflag, oflag, cflag, lflag, ispeed, ospeed, cc]
+    attr = termios.tcgetattr( fd )
+    print_tty_attr( attr )
+
+    attr[4] = termios.B57600
+    attr[5] = termios.B57600
+#    attr[4] = termios.B115200
+#    attr[5] = termios.B115200
+    termios.tcsetattr( fd, termios.TCSANOW, attr )
+    attr = termios.tcgetattr( fd )
+    print_tty_attr( attr )
+
+#    retcode = os.write( fd, "scan var print\r\n" )
+    #termios.tcflush( fd, termios.TCIOFLUSH )
+    #print retcode
+    while 1 :
+        buf = os.read( fd, 1 )
+        if opt_hex_output :
+            for c in buf :
+                sys.stdout.write( "%02x " % ord(c) )
+        else :
+            sys.stdout.write(buf)
+        sys.stdout.flush()
+
+    os.close( fd )
+
